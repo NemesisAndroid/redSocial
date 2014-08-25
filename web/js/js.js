f407fef190faf1fb2006a70373d0c155f35423d0
NemesisLalo <goliosandroy@gmail.com>
2014-08-24 23:22:51 -0300

    agregue loguin  y lyt_session.html.twig

---
+++ web/js/js.js
@@ -0,0 +1,7 @@
+//$(function() {
+//	$("#drt_asentamientosbundle_gente_cumple").datepicker({showOtherMonths: true,
+//	selectOtherMonths: true});
+//});
+
+//$( "#datepicker" ).datepicker( "option", "dateFormat", $( this ).val() );
+//});
